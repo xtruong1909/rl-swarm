cc641662299cb1c87018a374c3cf866288bb0c57
xtruong1909 <140407808+xtruong1909@users.noreply.github.com>
2025-05-02 11:31:40 +0700

    Update stage2_rewards.py

--- hivemind_exp/gsm8k/stage2_rewards.py
+++ hivemind_exp/gsm8k/stage2_rewards.py
@@ -65,18 +65,18 @@ def count_xml(text) -> float:
         return 0.0
     count = 0.0
     if text.count("<compare>\n") == 1:
-        count += 10
+        count += 100
     if text.count("\n</compare>\n") == 1:
-        count += 10
+        count += 100
     if text.count("<explain>\n") == 1:
-        count += 10
+        count += 100
     if text.count("\n</explain>\n") == 1:
-        count += 10
+        count += 100
     if text.count("\n<identify>\n") == 1:
-        count += 10
+        count += 100
         count -= len(text.split("\n</identify>\n")[-1]) * 0.001
     if text.count("\n</identify>") == 1:
-        count += 10
+        count += 100
         count -= (len(text.split("\n</identify>")[-1]) - 1) * 0.001
     return count
 
