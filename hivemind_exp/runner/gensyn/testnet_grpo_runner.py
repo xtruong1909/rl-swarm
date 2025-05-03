f39d1af68f8742c34abf2434b4085cfdcf57b779
xtruong1909 <140407808+xtruong1909@users.noreply.github.com>
2025-05-03 17:21:44 +0700

    Update testnet_grpo_runner.py

--- hivemind_exp/runner/gensyn/testnet_grpo_runner.py
+++ hivemind_exp/runner/gensyn/testnet_grpo_runner.py
@@ -3,13 +3,10 @@ from dataclasses import dataclass
 from functools import partial
 from typing import Callable, Tuple
 
-import hivemind
 from datasets import Dataset
 from trl import GRPOConfig, ModelConfig
 
-from hivemind_exp.chain_utils import (
-    SwarmCoordinator,
-)
+from hivemind_exp.chain_utils import SwarmCoordinator
 from hivemind_exp.runner.grpo_runner import GRPOArguments, GRPORunner
 from hivemind_exp.trainer.gensyn.testnet_grpo_trainer import TestnetGRPOTrainer
 
@@ -20,7 +17,11 @@ logger = logging.getLogger(__name__)
 class TestnetGRPOArguments:
     # Mutually exclusive.
     wallet_private_key: str | None = None  # EOA wallet private key.
-    modal_org_id: str | None = None # Modal organization ID.
+    modal_org_id: str | None = None        # Modal organization ID.
+
+    # Swarm coordinator contract address
+    contract_address: str = ""
+
 
 class TestnetGRPORunner(GRPORunner):
     def __init__(self, coordinator: SwarmCoordinator) -> None:
@@ -34,13 +35,8 @@ class TestnetGRPORunner(GRPORunner):
         self.coordinator.register_peer(peer_id)
 
     def setup_dht(self, grpo_args):
-        initial_peers = grpo_args.initial_peers
-
-        dht = hivemind.DHT(start=True, startup_timeout=30, **self._dht_kwargs(grpo_args))
-        logger.info(f"🐝 Joining swarm with initial_peers = {initial_peers}")
-
+        dht = super().setup_dht(grpo_args)
         peer_id = str(dht.peer_id)
-        self.name = self._get_animal_name(peer_id)
         self.register_peer(peer_id)
         return dht
 
@@ -60,13 +56,12 @@ class TestnetGRPORunner(GRPORunner):
             logger.info("Proceeding as bootnode!")
 
         grpo_args.initial_peers = initial_peers
+        # Use TestnetGRPOTrainer which forces self-submission
+        trainer_cls = partial(TestnetGRPOTrainer, coordinator=self.coordinator)
         super().run(
             model_args,
             grpo_args,
             training_args,
             initial_datasets_fn,
-            partial(
-                TestnetGRPOTrainer,
-                coordinator=self.coordinator
-            ),
+            trainer_cls,
         )
